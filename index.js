0c46f4384326494c79fcc547ab58bd57bb7c5218
Kevin Ho <kevinjho1996@gmail.com>
2021-01-18 16:49:43 -0500

    get all touched accounts from to and from

--- index.js
+++ index.js
@@ -38,41 +38,13 @@ const loadContract = (name, isL2) => {
 ;(async () => {
   const latestVerifierBlockNum = await verifierProvider.getBlockNumber()
   console.log('latestVerifierBlockNum', latestVerifierBlockNum)
-  let start = 0
-  let finish = latestVerifierBlockNum
-  while (start !== finish) {
-    let i = Math.floor((start + finish)/2)
-    const sequencerBlock = await l2Provider.send('eth_getBlockByNumber', [`0x${i.toString(16)}`, true])
+  let touchedAccounts = new Set()
+  for (let i = 1; i <= latestVerifierBlockNum; i++) {
     const verifierBlock = await verifierProvider.send('eth_getBlockByNumber', [`0x${i.toString(16)}`, true])
-    // if (sequencerBlock.stateRoot === verifierBlock.stateRoot) {
-    //   console.log(`matching state roots for block ${i}: ${sequencerBlock.stateRoot}`)
-    // } else {
-    //   console.error(`ERROR: MISMATCHED STATE ROOTS AT BLOCK ${i}. Sequencer has state root: ${sequencerBlock.stateRoot}. Verifier has state root:  ${verifierBlock.stateRoot}`)
-    // }
-    if (sequencerBlock.transactions[0].hash === verifierBlock.transactions[0].hash) {
-      start = i
-      console.log(`matching tx hashes for block ${i}: ${sequencerBlock.transactions[0].hash}`)
-    } else {
-      finish = i
-      console.error(`ERROR: MISMATCHED txs AT BLOCK ${i}. Sequencer has tx hash: ${sequencerBlock.transactions[0].hash}. Verifier has tx hash:  ${verifierBlock.transactions[0].hash}`)
-    }
+    touchedAccounts.add(verifierBlock.transactions[0].from)
+    touchedAccounts.add(verifierBlock.transactions[0].to)
   }
-  // const response = await axios.get(`${DEPLOYER_URL}/addresses.json`);
-  // l1Addresses = response.data;
-  // console.log('geth chain Id', (await l2Provider.getNetwork()).chainId)
-  // const ExecutionManager  = s loadContract('OVM_ExecutionManager', true)
-  // console.log('EM addr', ExecutionManager.address)
-  // console.log('executionManager chain id', (await ExecutionManager.ovmCHAINID()).toString())
-  // hexZeroPad(currentWallet, 32)
-  // const L2Messenger  = loadContract('OVM_L2CrossDomainMessenger', true)
-  // console.log(await L2Messenger.xDomainMessageSender())
-
-
-  // const filter = {
-  //   address: '0x5A081fa7e3483504385e7C804AB1509499e2A408',
-  //   topics: [id(`WithdrawalInitiated(address,uint256)`), hexZeroPad('0xe75103858b0b02b76DD78250442DE26e5Ac331D9', 32)],
-  //   fromBlock: 0,
-  // };
-  // const logs = await l2Provider.getLogs(filter);
-  // console.log('located in block #', logs[0].blockNumber)
+  touchedAccounts.delete(null) //remove `null` which is the `to` for Contract creations
+  const output = Array.from(touchedAccounts)
+  console.log(JSON.stringify(output))
 })()
